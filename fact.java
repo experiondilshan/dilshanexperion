e3af6195d34fbe2cdf872579dc0f1c6a24f573d1
experiondilshan <dilshannizar94@gmail.com>
2016-10-27 13:06:42 +0530

    file is committed

---
+++ fact.java
@@ -0,0 +1,17 @@
+class factorial
+{
+	public static void main(string args[])
+	{
+		System.out.println("Enter the number");
+		BufferedReader br=new BufferedReader(new InputStream Reader(System.in));
+		int a=interger.parseInt(br.readLine());
+		int b=a;
+		for(int i=a-1;i!=0;i--)
+		{
+			b=b*i;
+		}
+		System.out.println("factorial is:"+b);
+	}
+}
+
+			
